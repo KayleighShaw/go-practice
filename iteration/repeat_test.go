511b8c50932d44daaa043f185f2694acd9b43830
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-08-11 17:32:21 +0100

    Added a benchmark

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -10,3 +10,10 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+//Will measure how long the code takes to run, by default Benchmarks are run sequentially
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
