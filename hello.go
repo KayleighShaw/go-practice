55226f8cfbe0d6ed80d6b6b193bf04822b47cd0d
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-08-11 15:58:40 +0100

    Spanish and French prefixes to Hello func and tests

--- hello.go
+++ hello.go
@@ -7,16 +7,27 @@ import "fmt"
 // }
 
 //constants improve performance + saves you creating the "Hello, " string every time the Hello func is called
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefiex = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Domain - good to seperate your "domain" code from the outside world (side-effects)
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefiex + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
