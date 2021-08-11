bbe9d5b684cbcbf0b90aa8fe6efbfd3216a4555a
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-08-11 15:18:14 +0100

    Subtests added and helper method

--- hello.go
+++ hello.go
@@ -6,9 +6,15 @@ import "fmt"
 // 	fmt.Println("Hello, world!")
 // }
 
+//constants improve performance + saves you creating the "Hello, " string every time the Hello func is called
+const englishHelloPrefix = "Hello, "
+
 // Domain - good to seperate your "domain" code from the outside world (side-effects)
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
