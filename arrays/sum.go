0682a978d836aef551eda4b388283fc955476ad5
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-08-12 11:23:18 +0100

    Extra tests and refactoring for sum function

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,6 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	// range returns two values - the index and the value. The blank identifier ignores the index value
 	for _, number := range numbers {
