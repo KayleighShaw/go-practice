1b4ee9498838a4d3a681612b7b592b98f029eeff
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-01 09:15:10 +0100

    Tests pass with Shape interface

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,12 +2,17 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 // A struct is a named collection of fields where you can store data
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+// Rectangle has a method called Area that returns a float64 so it satisfies the Shape interface
 // This is a method: func (receiverName ReceieverType) MethodName(args)
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
@@ -20,11 +25,3 @@ type Circle struct {
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
-
-// func Perimeter(rectangle Rectangle) float64 {
-// 	return 2 * (rectangle.Width + rectangle.Height)
-// }
-
-// func Area(rectangle Rectangle) float64 {
-// 	return rectangle.Width * rectangle.Height
-// }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,46 +2,24 @@ package structs
 
 import "testing"
 
-// func TestPerimeter(t *testing.T) {
-// 	rectangle := Rectangle{10.0, 10.0}
-// 	got := Perimeter(rectangle)
-// 	want := 40.0
-
-// 	if got != want {
-// 		t.Errorf("got %.2f want %.2f", got, want)
-// 	}
-// }
-// func TestArea(t *testing.T) {
-// 	rectangle := Rectangle{12.0, 6.0}
-// 	got := Area(rectangle)
-// 	want := 72.0
-
-// 	if got != want {
-// 		t.Errorf("got %.2f want %.2f", got, want)
-// 	}
-// }
-
 func TestArea(t *testing.T) {
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
-
+	//asking for a Shape to be passed it. If it isn't a shape, it will not compile
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		// f replaced by g = will print a more precise decimal number in the error message
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
-
 }
