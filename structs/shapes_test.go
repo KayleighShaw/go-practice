b9d08234b54bc69eb377c4fb21545774cef665c1
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-06 08:59:38 +0100

    Add Triangle to test table

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -10,6 +10,7 @@ func TestArea(t *testing.T) {
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	// iterate over them like any other slice using the struct fields
