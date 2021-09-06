7631b75e0f2b7ef35537966fe36b537bb6f01eaa
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-06 08:55:32 +0100

    Refactor: intro to table tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,7 @@ package structs
 import "testing"
 
 func TestArea(t *testing.T) {
-
+	// creating an anonymous struct (areaTests) with the fields 'shape' and 'want'
 	areaTests := []struct {
 		shape Shape
 		want  float64
@@ -12,29 +12,11 @@ func TestArea(t *testing.T) {
 		{Circle{10}, 314.1592653589793},
 	}
 
+	// iterate over them like any other slice using the struct fields
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
 		if got != tt.want {
 			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
-
-	//asking for a Shape to be passed it. If it isn't a shape, it will not compile
-	// checkArea := func(t *testing.T, shape Shape, want float64) {
-	// 	t.Helper()
-	// 	got := shape.Area()
-	// 	if got != want {
-	// 		t.Errorf("got %g want %g", got, want)
-	// 	}
-	// }
-
-	// t.Run("rectangles", func(t *testing.T) {
-	// 	rectangle := Rectangle{12, 6}
-	// 	checkArea(t, rectangle, 72.0)
-	// })
-
-	// t.Run("circles", func(t *testing.T) {
-	// 	circle := Circle{10}
-	// 	checkArea(t, circle, 314.1592653589793)
-	// })
 }
