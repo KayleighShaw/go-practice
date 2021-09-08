b4c655fb927a5445e93ca052dc7c77dd7cb9c522
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-08 09:40:40 +0100

    Test: formatted for string

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -20,3 +20,11 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,17 +6,19 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+
 	wallet := Wallet{}
 
 	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
 	want := Bitcoin(10)
 
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
+	// formats for %s (string)
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
