d9e4bf0870489f332b05395ed88098d8d364c1a4
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-14 09:24:57 +0100

    Map/Dictionary test and basic functions added

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,9 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	// getting a value out of a map is the same as getting a value out of an array - map[key]
-	return dictionary[word]
+// Dictoinary type acts as a thin wrapper around map
+type Dictionary map[string]string
+
+// with the custom type defined we can create the Search method
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,17 +4,19 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	// declare a map with the map keyword, requires the [key] type and the value type after []
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "this is just a test"
 
 	assertString(t, got, want)
 }
 
+// helps make the implementation more general
 func assertString(t testing.TB, got, want string) {
 	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
