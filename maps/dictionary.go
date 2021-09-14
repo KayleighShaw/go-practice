cda663e9eb1928d3bf19f9ba27a78a6a81c0b948
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-14 12:49:17 +0100

    Added a Delete word function and corresponding test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,8 +4,9 @@ type Dictionary map[string]string
 
 // this makes the errors more reusable and immutable
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -35,4 +36,24 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// go has a build-in function delete that works on maps - takes map argument and key to be removed argument
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
