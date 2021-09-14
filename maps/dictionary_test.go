5d55329bfc760f7a5454979e37cf55c9864ecde1
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-14 12:30:02 +0100

    Feat: Add word function added with corresponding test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+// Tests
 func TestSearch(t *testing.T) {
 	// declare a map with the map keyword, requires the [key] type and the value type after []
 	dictionary := Dictionary{"test": "this is just a test"}
@@ -24,15 +25,29 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
+// Helper Functions
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
@@ -48,9 +63,8 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
-
 	if got != want {
-		t.Errorf("got error %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
 
