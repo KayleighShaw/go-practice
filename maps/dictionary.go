841f6a1131f1a52033d086af9ad01244bf728cb5
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-09-14 12:34:45 +0100

    Refactor: refactiored to make the errors more reusable

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,15 +1,18 @@
 package main
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+// this makes the errors more reusable and immutable
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
